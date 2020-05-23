94ec071a29fec63462eda5b2f91b5ab8d44dcb3d
Jacob Bates <jacob.bates@gmail.com>
2020-05-23 13:47:23 -0400

    Update 1080, add 720

--- rec720.sh
+++ rec720.sh
@@ -3,7 +3,7 @@
 echo -n "Filename (minus the extension):"
 read FILENAME
 
-raspivid -o $FILENAME.h264 -t 0 -n -w 1920 -h 1080 -b 17000000 --framerate 30 --keypress -v
+raspivid -o $FILENAME.h264 -t 0 -n -w 1280 -h 720 -b 10000000 --framerate 30 --keypress -v
 
 MP4Box -add $FILENAME.h264 -fps 30 $FILENAME.mp4
 
