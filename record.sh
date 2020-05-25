69c6f3729c85584184bd348862bb2ded996b3c88
Jacob Bates <jacob.bates@gmail.com>
2020-05-25 13:10:35 -0400

    Combined record scripts, created config file

---
+++ record.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source config.sh
+
+if [ "$VERBOSE" = true ] ; then
+	VERBOSE_STR="-v"
+else
+	VERBOSE_STR=""
+fi
+
+echo -n "Filename (minus the extension):"
+read FILENAME
+
+raspivid -o $FILENAME.h264 -t 0 -n -w $WIDTH -h $HEIGHT -b $BITRATE_1080 --framerate $FPS --keypress $VERBOSE_STR
+
+MP4Box -add $FILENAME.h264 -fps $FPS $FILENAME.mp4
+
+if [ "$REMOVE_H264" = true ] ; then
+	rm $FILENAME.h264
+	echo "Removing $FILENAME.h264"
+fi
