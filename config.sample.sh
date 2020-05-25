93405214495226bfbc9d82804cc9096a28c55fe9
Jacob Bates <jacob.bates@gmail.com>
2020-05-25 13:31:51 -0400

    Unify bitrate setting, document config file

--- config.sample.sh
+++ config.sample.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+#Settings for 720p video
+#WIDTH=1280
+#HEIGHT=720
+#BITRATE=10000000
+
+#Settings for 1080p video
 WIDTH=1920
 HEIGHT=1080
+BITRATE=17000000
+
+#Frames per second for video capture
 FPS=30
+
+#Verbose output from raspivid
 VERBOSE=true
-BITRATE_1080=17000000
-BITRATE_720=10000000
+
+#Remove .h264 file after conversion to .mp4 is complete
 REMOVE_H264=true
--- record.sh
+++ record.sh
@@ -11,7 +11,7 @@ fi
 echo -n "Filename (minus the extension):"
 read FILENAME
 
-raspivid -o $FILENAME.h264 -t 0 -n -w $WIDTH -h $HEIGHT -b $BITRATE_1080 --framerate $FPS --keypress $VERBOSE_STR
+raspivid -o $FILENAME.h264 -t 0 -n -w $WIDTH -h $HEIGHT -b $BITRATE --framerate $FPS --keypress $VERBOSE_STR
 
 MP4Box -add $FILENAME.h264 -fps $FPS $FILENAME.mp4
 
